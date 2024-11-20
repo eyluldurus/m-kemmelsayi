5353498e992190eb0a640bf95af67486802287a6
eyluldurus <eyll.durus@gmail.com>
2024-11-20 22:06:05 +0300

    Add files via upload

---
+++ mukemmel sayi odevi.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("bir sayı girin: ");
+        int sayi = int.Parse(Console.ReadLine());
+
+        int toplam = 0;
+
+        for (int i = 1; i < sayi; i++)
+        {
+            if (sayi % i == 0)
+            {
+                toplam += i;
+            }
+        }
+
+        if (toplam == sayi)
+        {
+            Console.WriteLine($"{sayi} bir mükemmel sayıdır.");
+        }
+        else
+        {
+            Console.WriteLine($"{sayi} bir mükemmel sayı değildir.");
+        }
+    }
+}
